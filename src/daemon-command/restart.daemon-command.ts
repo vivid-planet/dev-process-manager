9f6b5b43874e87a8e6463fe03ca5216328cc3c46
Niko Sams <ns@vivid-planet.com>
2022-09-30 10:38:56 +0200

    Fix restart

--- src/daemon-command/restart.daemon-command.ts
+++ src/daemon-command/restart.daemon-command.ts
@@ -9,6 +9,7 @@ export async function restartDaemonCommand(daemon: Daemon, socket: Socket, scrip
     const scriptsToProcess = scriptsMatchingPattern(daemon, scriptName);
 
     for (const script of scriptsToProcess) {
+        daemon.scriptStatus[script.name] = "stopping";
         await killProcess(daemon, socket, script.name);
 
         console.log(`starting ${script.name}`);
