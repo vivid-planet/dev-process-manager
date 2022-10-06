f152f95b22ceb2f49291e1519be8ba9071f2b2d3
Niko Sams <ns@vivid-planet.com>
2022-10-06 08:36:20 +0200

    Show error when invalid script name was given

--- src/daemon-command/restart.daemon-command.ts
+++ src/daemon-command/restart.daemon-command.ts
@@ -5,6 +5,11 @@ import { scriptsMatchingPattern } from "./scripts-matching-pattern";
 
 export async function restartDaemonCommand(daemon: Daemon, socket: Socket, names: string[]): Promise<void> {
     const scriptsToProcess = scriptsMatchingPattern(daemon, names);
+    if (!scriptsToProcess.length) {
+        socket.write("No matching scripts found in dev-pm config");
+        socket.end();
+        return;
+    }
 
     for (const script of scriptsToProcess) {
         script.status = "stopping";
