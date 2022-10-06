43d5ef7eb6f1752adf791b7ef2136d417823bc52
Niko Sams <ns@vivid-planet.com>
2022-10-06 09:01:50 +0200

    Add group to example config

--- example/dev-pm.config.js
+++ example/dev-pm.config.js
@@ -3,10 +3,12 @@ module.exports = {
         {
             name: "sleep-1",
             script: "sleep 60",
+            group: "sleep",
         },
         {
             name: "sleep-2",
             script: "sleep 60",
+            group: "sleep",
         },
     ],
 };
