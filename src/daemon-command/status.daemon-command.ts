fcdfe01a3a35d1e1db27134cf3a8c74a4cee1e5e
Niko Sams <ns@vivid-planet.com>
2022-10-29 09:26:46 +0200

    Add cpu/memory usage to status output

--- src/daemon-command/status.daemon-command.ts
+++ src/daemon-command/status.daemon-command.ts
@@ -1,11 +1,13 @@
 import CLITable from "cli-table3";
 import colors from "colors";
 import { Socket } from "net";
+import pidusage from "pidusage";
+import prettyBytes from "pretty-bytes";
 
 import { Daemon } from "../commands/start-daemon.command";
 import { scriptsMatchingPattern } from "./scripts-matching-pattern";
 
-export function statusDaemonCommand(daemon: Daemon, socket: Socket, names: string[]): void {
+export async function statusDaemonCommand(daemon: Daemon, socket: Socket, names: string[]): Promise<void> {
     const scriptsToProcess = scriptsMatchingPattern(daemon, names);
     if (!scriptsToProcess.length) {
         socket.write("No matching scripts found in dev-pm config");
@@ -14,16 +16,22 @@ export function statusDaemonCommand(daemon: Daemon, socket: Socket, names: strin
     }
 
     const table = new CLITable({
-        head: [colors.blue.bold("Script"), colors.blue.bold("Status"), colors.bold.blue("PID")],
-        colWidths: [100, 20, 20],
+        head: [colors.blue.bold("Script"), colors.blue.bold("Status"), colors.blue.bold("CPU"), colors.blue.bold("Mem"), colors.bold.blue("PID")],
         style: { compact: true },
     });
-    scriptsToProcess.forEach((script) => {
+    for (const script of scriptsToProcess) {
         const running = script.process ? !script.process.killed : false;
         const status = script.status === "waiting" ? colors.yellow("Waiting") : running ? colors.green("Running") : colors.red("Stopped");
         const pid = script.process && !script.process.killed ? script.process.pid : undefined;
-        table.push([script.name, status, pid?.toString()]);
-    });
+        let cpu = "";
+        let memory = "";
+        if (pid) {
+            const stats = await pidusage(pid);
+            cpu = `${stats.cpu}%`;
+            memory = prettyBytes(stats.memory);
+        }
+        table.push([script.name, status, cpu, memory, pid?.toString()]);
+    }
     socket.write(table.toString());
     socket.end();
 }
