e06211f42c16b656d275a20d738f2be4d5fe27b2
Niko Sams <ns@vivid-planet.com>
2022-11-07 16:16:56 +0100

    fix merge error

--- src/commands/status.command.ts
+++ src/commands/status.command.ts
@@ -1,7 +1,7 @@
 import { StatusCommandOptions } from "../daemon-command/status.daemon-command";
 import { connect } from "./connect";
 
-export const status = async (names: string[]): Promise<void> => {
+export const status = async (options: StatusCommandOptions): Promise<void> => {
     const client = await connect();
     client.write(`status ${JSON.stringify(options)}`);
 };
