6ee75f942a336875f96df689a13371a9e980c100
Niko Sams <ns@vivid-planet.com>
2022-11-08 17:06:32 +0100

    Wait a max time of 120 sec between restarts

--- src/daemon-command/script.ts
+++ src/daemon-command/script.ts
@@ -143,7 +143,7 @@ export class Script {
                 this.handleLogs(`[dev-pm] process crashed, restarting...`);
                 this.restartCount++;
                 this.status = "backoff";
-                const waitTime = Math.pow(1.3, this.restartCount);
+                const waitTime = Math.min(Math.pow(1.3, this.restartCount), 120);
                 this.handleLogs(`[dev-pm] waiting ${Math.round(waitTime)}s between restarts`);
                 await new Promise((r) => setTimeout(r, waitTime * 1000));
                 if (this.status == "backoff") {
