8f65aafbc572b200e041c222fa7f546dbaed1f3d
Johannes Obermair <johannes.obermair@vivid-planet.com>
2025-10-07 13:16:59 +0200

    Fix lint-staged setup

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,8 @@
-module.exports = {
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
     "*.{ts,tsx,js,jsx,json,css,scss,md}": () => "npx yarn lint:eslint",
     "*.{ts,tsx}": () => "npx yarn lint:tsc",
 };
